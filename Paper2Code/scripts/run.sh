852709d98b9a5dc3477424d5330244e9de361764
yyj20251029 <yangyujie343@gmail.com>
2025-12-11 11:59:00 -0500

    Add files via upload

---
+++ Paper2Code/scripts/run.sh
@@ -0,0 +1,50 @@
+# export OPENAI_API_KEY=""
+
+GPT_VERSION="o3-mini"
+
+PAPER_NAME="Transformer"
+PDF_PATH="../examples/Transformer.pdf" # .pdf
+PDF_JSON_PATH="../examples/Transformer.json" # .json
+PDF_JSON_CLEANED_PATH="../examples/Transformer_cleaned.json" # _cleaned.json
+OUTPUT_DIR="../outputs/Transformer"
+OUTPUT_REPO_DIR="../outputs/Transformer_repo"
+
+mkdir -p $OUTPUT_DIR
+mkdir -p $OUTPUT_REPO_DIR
+
+echo $PAPER_NAME
+
+echo "------- Preprocess -------"
+
+python ../codes/0_pdf_process.py \
+    --input_json_path ${PDF_JSON_PATH} \
+    --output_json_path ${PDF_JSON_CLEANED_PATH} \
+
+
+echo "------- PaperCoder -------"
+
+python ../codes/1_planning.py \
+    --paper_name $PAPER_NAME \
+    --gpt_version ${GPT_VERSION} \
+    --pdf_json_path ${PDF_JSON_CLEANED_PATH} \
+    --output_dir ${OUTPUT_DIR}
+
+
+python ../codes/1.1_extract_config.py \
+    --paper_name $PAPER_NAME \
+    --output_dir ${OUTPUT_DIR}
+
+cp -rp ${OUTPUT_DIR}/planning_config.yaml ${OUTPUT_REPO_DIR}/config.yaml
+
+python ../codes/2_analyzing.py \
+    --paper_name $PAPER_NAME \
+    --gpt_version ${GPT_VERSION} \
+    --pdf_json_path ${PDF_JSON_CLEANED_PATH} \
+    --output_dir ${OUTPUT_DIR}
+
+python ../codes/3_coding.py  \
+    --paper_name $PAPER_NAME \
+    --gpt_version ${GPT_VERSION} \
+    --pdf_json_path ${PDF_JSON_CLEANED_PATH} \
+    --output_dir ${OUTPUT_DIR} \
+    --output_repo_dir ${OUTPUT_REPO_DIR} \
